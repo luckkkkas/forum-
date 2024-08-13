45547f4e6f3f95ab663a390786e0f8fc8859d52b
lucas oliveira <lucashbo321@gmail.com>
2024-08-13 10:26:28 -0300

    implementação de validação de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,49 +1,47 @@
 import { Box, Button, Input, SimpleGrid, Text } from "@chakra-ui/react"
-import { useState } from "react"
-import { validateUser } from "../services/validateUser";
-import { useNavigate } from "react-router-dom";
-import { changeLocalStorage } from "../services/storage/storage";
+import { useForm , SubmitHandler } from "react-hook-form" 
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+interface IFormInput {
+    email: string
+    password: string
+  }
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().required().min(8),
+  })
+  .required()
 
 export const Login = () => {
-
-    const [email, setEmail] = useState('lucas@mail.com');
-    const [password, setPassword] = useState('123');
-    const navigate = useNavigate();
-    let [emailInvalid, setInvalidEmail] = useState(false)
-    let [passInvalid, setInvalidPass] = useState(false)
-
-    const validUser = async () => {
-        if (!await validateUser(email, password)) {
-            setInvalidEmail(true)
-            setInvalidPass(true)
-            return null
-        }
-        
-        navigate('/');
-        changeLocalStorage('valid', true);
-    }
+    
+    const { register , handleSubmit, formState: { errors } } = useForm({resolver: yupResolver(schema)})   
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+    
 
 
     return (
         <Box h={'100vh'} >
             <SimpleGrid w={'50vw'} m={'auto'} pt={'30vh'} >
-                <Input
-                    placeholder="Email or User"
-                    variant="flushed"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    type="email"
-                />
-                {emailInvalid ? <Text fontSize='xs' color={'red'}>email invalid</Text> : null}
-                <Input
-                    placeholder="Password"
-                    variant="flushed"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    type="password"
-                />
-                {passInvalid ? <Text fontSize='xs' color={'red'}>Password invalid</Text> : null}
-                <Button mt={10} onClick={() => validUser()} colorScheme="yellow" >Login</Button>
+                <form onSubmit = { handleSubmit ( onSubmit as any) } >
+                    <Input
+                        placeholder="Email or User"
+                        variant="flushed"
+                        type="email"
+                        {...register('email', {required:true})}
+                    />
+                    <Text color={"red"}>{errors.email?.message}</Text>
+                    <Input
+                        placeholder="Password"
+                        variant="flushed"
+                        type="password"
+                        {...register('password', {required: true})}
+                    />
+                    <Text color={'red'}>{errors.password?.message}</Text>
+                    <Button colorScheme="blue" type="submit" >Login</Button>
+                </form>
             </SimpleGrid>
         </Box>
     )
